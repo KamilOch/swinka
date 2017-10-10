54d08fa651777890e9ef04b00b00b3216f5cde8b
Kamil Ochnik <ilmak@o2.pl>
2017-10-10 17:40:17 +0200

    Java course, exercises 12 ( in work)

---
+++ src/main/java/it/morfoza/cwiczenia/Cwiczenie12.java
@@ -0,0 +1,19 @@
+package it.morfoza.cwiczenia;
+
+//Tablice jednowymiarowe
+
+public class Cwiczenie12 {
+    public static void main ( String [] args) {
+
+        int[] tablica = new int[10];
+
+        for (int i=0; i<10; i++ );
+           tablica[i] =i+1;
+
+         int zmienna = tablica[3];
+
+        for (int i=0; i<10; i++);
+            System.out.println("Kolejna komórka to: "+ tablica[i]);
+
+    }
+}
