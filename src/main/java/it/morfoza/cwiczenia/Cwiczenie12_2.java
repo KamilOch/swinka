48be6e639374cbf5b7ead05298c8aa1456c7adc8
Kamil Ochnik <ilmak@o2.pl>
2017-10-11 13:40:12 +0200

    Java course, homework 12_2

---
+++ src/main/java/it/morfoza/cwiczenia/Cwiczenie12_2.java
@@ -0,0 +1,25 @@
+package it.morfoza.cwiczenia;
+
+//Tablice jednowymiarowe praca domowa
+
+import java.util.Scanner;
+
+public class Cwiczenie12_2 {
+    public static void main (String[] args) {
+
+        Scanner odczyt = new Scanner(System.in);
+        int n= odczyt.nextInt();
+
+       int[] tablica = new int[n];
+
+
+       for (int i=0; i<n; i++ )
+            tablica[i] =i+1;
+
+        int licznik =0;
+       while ( licznik < n) {
+           System.out.println("Kolejna komórka to: " + licznik);
+           licznik++;
+       }
+    }
+}
