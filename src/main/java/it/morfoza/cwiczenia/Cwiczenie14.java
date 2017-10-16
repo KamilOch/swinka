f6747ab47da439fba2daf4b56b3cd3d72ae6d477
Kamil Ochnik <ilmak@o2.pl>
2017-10-16 12:13:57 +0200

    Java course, exercise 14

---
+++ src/main/java/it/morfoza/cwiczenia/Cwiczenie14.java
@@ -0,0 +1,18 @@
+package it.morfoza.cwiczenia;
+
+//Pętla for each
+
+public class Cwiczenie14 {
+    public static void main (String[] args) {
+
+        int[] tablica = new int[10];
+         //wypełnienie tablicy
+        for(int i=0; i<10; i++)
+        tablica[i] = i+1;
+
+        //wyświetlenie przy użyciu pętli for each
+        for(int x : tablica)
+        System.out.println(x);
+
+    }
+}
