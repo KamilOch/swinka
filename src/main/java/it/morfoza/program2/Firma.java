84f3829edc7cc6ef5933238a2a27017accb6d752
Kamil Ochnik <ilmak@o2.pl>
2017-10-18 17:28:24 +0200

    Add new Program 1 and 2

---
+++ src/main/java/it/morfoza/program2/Firma.java
@@ -0,0 +1,20 @@
+package it.morfoza.program2;
+
+public class Firma {
+    public static void main (String args[]){
+       Pracownik pracownik1 = new Pracownik();
+       Pracownik pracownik2 = new Pracownik();
+       pracownik1.imie = "Roman";
+       pracownik1.nazwisko = "Kowalski";
+       pracownik1.wiek = 40;
+
+       pracownik2.imie = "Włodek";
+       pracownik2.nazwisko = "Zięba";
+       pracownik2.wiek = 44;
+
+       System.out.println("Pracownicy firmy to: \n");
+       System.out.println("Imię "+(pracownik1.imie)+"\nNazwisko "+(pracownik1.nazwisko)+"\nWiek "+(pracownik1.wiek ));
+
+       System.out.println("\nImię "+(pracownik2.imie)+"\nNazwisko "+(pracownik2.nazwisko)+"\nWiek "+(pracownik2.wiek ));
+    }
+}
---
+++ src/main/java/it/morfoza/program2/Pracownik.java
@@ -0,0 +1,8 @@
+package it.morfoza.program2;
+
+public class Pracownik {
+
+    String imie = "imie";
+    String nazwisko = "nazwisko";
+    int wiek;
+}
