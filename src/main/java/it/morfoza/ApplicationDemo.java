1774b6d3e41ecea6615d8c620af4a81973e7f88e
il <ilmak@o2.pl>
2018-11-02 21:56:09 +0100

    add ApplicationDemo

---
+++ src/main/java/it/morfoza/ApplicationDemo.java
@@ -0,0 +1,16 @@
+package it.morfoza;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApplicationDemo {
+
+    public static void main(String[] args) {
+        System.setProperty("spring.profiles.active", "demo");
+        System.setProperty("JDBC_DATABASE_URL", "jdbc");
+        Application.main(args);
+    }
+
+}
