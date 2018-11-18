36b3a252ddb088ad3294193df0c6b6876a3cc4f1
Kamil Ochnik <ilmak@o2.pl>
2018-11-18 19:07:57 +0100

    add greeting

--- src/main/java/it/morfoza/CalculatorControler.java
+++ src/main/java/it/morfoza/CalculatorControler.java
@@ -2,6 +2,8 @@ package it.morfoza;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CalculatorControler {
@@ -9,6 +11,20 @@ public class CalculatorControler {
     private Calculator calculator;
 
 
+
+    @RequestMapping("/podajImie")
+    public String pobieranieImienia(){
+        return "podajImie";
+    }
+
+    @RequestMapping("/powitanie")
+    public String powitanie (
+            String imie,
+            Model model
+    ){
+        model.addAttribute("imie", imie);
+        return "powitanie";
+    }
 
 
 }
